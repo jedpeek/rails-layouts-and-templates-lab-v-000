e7bca57a9042fe60926806f5facaddfe2424f7eb
Jed Peek <jedpeek@yahoo.com>
2018-08-23 21:53:22 +0000

    automatically backed up by learn

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -3,4 +3,12 @@ class StoreAdminController < ApplicationController
   def home
     render :layout => "admin"
   end
+
+  def orders
+    render :layout => "orders"
+  end
+
+  def invoice
+    render :layout => "invoice"
+  end
 end
